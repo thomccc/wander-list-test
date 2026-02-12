4a69b0d75f80bf5740e5571df606123cae07bd7d
thomccc <thomas.arsenal.chen@gmail.com>
2026-02-12 14:51:58 -0800

    Load Wander design system stylesheet in app entry

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "@wandercom/design-system-web/styles.css"
 import './index.css'
 import App from './App.tsx'
 
